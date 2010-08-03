463575d9eaafca2a797ab785da02e849717cb171
Philipp Kraus <philipp.kraus@flashpixx.de>
2010-08-03 16:02:59 +0000

    licence

--- tools/files/hdf.hpp
+++ tools/files/hdf.hpp
@@ -3,6 +3,7 @@
  #########################################################################
  # GPL License                                                           #
  #                                                                       #
+ # This file is part of the Machine Learning Framework.                  #
  # Copyright (c) 2010, Philipp Kraus, <philipp.kraus@flashpixx.de>       #
  # This program is free software: you can redistribute it and/or modify  #
  # it under the terms of the GNU General Public License as published by  #
