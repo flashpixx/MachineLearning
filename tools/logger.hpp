fe678517b54247e2a4baff78ab3468898a2dd408
Philipp Kraus <philipp.kraus@flashpixx.de>
2010-12-02 12:06:37 +0000

    logger

--- tools/logger.hpp
+++ tools/logger.hpp
@@ -53,8 +53,13 @@ namespace machinelearning { namespace tools {
     /** logger class for writing log information 
      * @note for MPI using every process must call startListener and shutdownListener for synchronize the CPUs and the tag for logtargets ist set with a preprocessor flag (LOGGER_MPI_TAG).
      * The singletone object works only "between" the calls start- and shutdownListener because the MPI object must exists.
-     * @note If the listener function is used, you don't use the Boost::MPI::Environmental initialisation. Use the default MPI calls of the mpi.h (MPI::Init_thread( argc, argv, MPI_THREAD_SERIALIZED ) &
-     * MPI::Finalize())
+     * @note If the listener function is used, you don't use the Boost::MPI::Environmental initialisation. Use the default MPI calls of the mpi.h: 
+     * @code
+        #include <mpi.h>
+        MPI::Init_thread( argc, argv, MPI_THREAD_SERIALIZED )
+        MPI::Finalize())
+     * @endcode
+     * The MPI libraries must be compiled with multithread support
      * @todo tmpnam(NULL) change to a safe variante (posix mkstemp(), but this isn't standard)
      **/
     class logger {
