0520e0c82a02ecae1fd8b5dc090a7d0003c050ec
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-03-04 14:06:38 +0000

    docu

--- tools/sources/sources.h
+++ tools/sources/sources.h
@@ -32,6 +32,8 @@ namespace machinelearning {
         /** namespace with classes for generating example data
          * $LastChangedDate$
          * @todo adding support for OpenStreetMap http://wiki.openstreetmap.org/wiki/API
+         * @todo adding support for Apache Cassandra http://en.wikipedia.org/wiki/Apache_Cassandra
+         * @todo adding support for mySQL / MS SQL / Postgres SQL / Oracle
          **/
         namespace sources {};
     
