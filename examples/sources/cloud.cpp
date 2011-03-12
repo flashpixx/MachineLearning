fb76aca583f8d91c5d4f42f831f0e0468f42443c
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-03-12 23:52:02 +0000

    cloud example

--- examples/sources/cloud.cpp
+++ examples/sources/cloud.cpp
@@ -21,71 +21,150 @@
  @endcond
  **/
 
+#include <map>
+
 #include <machinelearning.h>
+#include <boost/any.hpp>
 #include <boost/lexical_cast.hpp>
 
 using namespace machinelearning;
 
 
+
+bool cliArguments( int argc, char* argv[], std::map<std::string, boost::any>& p_args ) {
+    
+    if (argc < 2) {
+        std::cout << "--outfile" << "\t" << "output HDF5 file" << std::endl;
+        std::cout << "--outpath" << "\t" << "output path within the HDF5 file" << std::endl;
+        std::cout << "--dimension" << "\t" << "number of dimension (default: 2)" << std::endl;
+        std::cout << "--sampling" << "\t" << "sampling within the intervall (default: 10)" << std::endl;
+        std::cout << "--pointsmin" << "\t" << "minimal number of points (default: 50)" << std::endl;
+        std::cout << "--pointsmax" << "\t" << "maximal number of points (default: 500)" << std::endl;
+        std::cout << "--rangemin" << "\t" << "minimal range value (default: 0)" << std::endl;
+        std::cout << "--rangemax" << "\t" << "maximal range value (default: 1)" << std::endl;
+        std::cout << "--variancemin" << "\t" << "minimal variance value (default: 0.1)" << std::endl;
+        std::cout << "--variancemax" << "\t" << "maximal variance value (default: 0.8)" << std::endl;
+        
+        return false;
+    }
+    
+    // set default arguments
+    std::map<std::string, std::vector<std::string> > l_argmap;
+    l_argmap["outfile"]     = std::vector<std::string>();
+    l_argmap["outpath"]     = std::vector<std::string>();
+    l_argmap["dimension"]   = std::vector<std::string>();
+    l_argmap["sampling"]    = std::vector<std::string>();
+    l_argmap["pointsmin"]   = std::vector<std::string>();
+    l_argmap["pointsmax"]   = std::vector<std::string>();
+    l_argmap["rangemin"]    = std::vector<std::string>();
+    l_argmap["rangemax"]    = std::vector<std::string>();
+    l_argmap["variancemin"] = std::vector<std::string>();
+    l_argmap["variancemax"] = std::vector<std::string>();
+    
+    
+    // read all arguments
+    std::size_t n=1;
+    for(int i=1; i < argc; i+=n) {
+        n = 1;
+        std::string lc(argv[i]);
+        if (lc.length() < 2)
+            continue;
+        
+        lc = lc.substr(2);
+        boost::to_lower( lc );
+        
+        if (l_argmap.find(lc) != l_argmap.end()) {
+            std::vector<std::string> lv;
+            
+            for(int l=i+1; l < argc; ++l) {
+                std::string lc2(argv[l]);
+                if ( (lc2.length() >= 2) && (lc2.substr(0,2) == "--") )
+                    break;
+                
+                lv.push_back(lc2);
+                n++;
+            }
+            
+            l_argmap[lc] = lv;
+        }
+        
+    }
+    
+    
+    //check map values and convert them
+    if ( (l_argmap["outfile"].size() != 1) ||
+         (l_argmap["outpath"].size() != 1)
+       )
+        throw std::runtime_error("number of arguments are incorrect");
+    
+    p_args["outfile"]       = l_argmap["outfile"][0];
+    p_args["outpath"]       = l_argmap["outpath"][0];
+    
+    // default values
+    p_args["dimension"]     = std::size_t(2);
+    p_args["sampling"]      = std::size_t(10);
+    p_args["pointsmin"]     = std::size_t(50);
+    p_args["pointsmax"]     = std::size_t(500);
+    
+    p_args["rangemin"]      = double(0);
+    p_args["rangemax"]      = double(1.0);
+    p_args["variancemin"]   = double(0.1);
+    p_args["variancemax"]   = double(0.8);
+    
+    try {
+
+        if (l_argmap["dimension"].size() == 1)
+            p_args["dimension"] = boost::lexical_cast<std::size_t>(l_argmap["dimension"][0]);
+        
+        if (l_argmap["sampling"].size() == 1)
+            p_args["sampling"] = boost::lexical_cast<std::size_t>(l_argmap["sampling"][0]);
+        
+        if (l_argmap["pointsmin"].size() == 1)
+            p_args["pointsmin"] = boost::lexical_cast<std::size_t>(l_argmap["pointsmin"][0]);
+        if (l_argmap["pointsmax"].size() == 1)
+            p_args["pointsmax"] = boost::lexical_cast<std::size_t>(l_argmap["pointsmax"][0]);
+        
+        if (l_argmap["rangemin"].size() == 1)
+            p_args["rangemin"] = boost::lexical_cast<double>(l_argmap["rangemin"][0]);
+        if (l_argmap["rangemax"].size() == 1)
+            p_args["rangemax"] = boost::lexical_cast<double>(l_argmap["rangemax"][0]);
+        
+        if (l_argmap["variancemin"].size() == 1)
+            p_args["variancemin"] = boost::lexical_cast<double>(l_argmap["variancemin"][0]);
+        if (l_argmap["variancemax"].size() == 1)
+            p_args["variancemax"] = boost::lexical_cast<double>(l_argmap["variancemax"][0]);
+       
+
+    } catch (...) {
+        throw std::runtime_error("numerical data can not extracted");
+    }  
+    
+    return true;
+}
+
+
+
 int main(int argc, char* argv[]) {
     
-    // set default values
-    std::size_t dim                 = 0;
-    std::size_t sampling            = 10;
-    std::size_t pointsmin           = 50;
-    std::size_t pointsmax           = 500;
-    double rangemin                 = 0;
-    double rangemax                 = 1;
-    double variancemin              = 0.1;
-    double variancemax              = 0.8;
-    
-    // check min arguments
-    if (argc < 2) 
-        throw std::runtime_error("number of parameters not correct");
-
-    // try to convert string arguments to numeric values
-    try {
-        dim             = boost::lexical_cast<std::size_t>(argv[1]);
-        
-        if (argc > 2) 
-            sampling    = boost::lexical_cast<std::size_t>(argv[2]);
-        
-        if (argc > 3)
-            pointsmin   = boost::lexical_cast<std::size_t>(argv[3]);
-        if (argc > 4)
-            pointsmax   = boost::lexical_cast<std::size_t>(argv[4]);
-        
-        if (argc > 5)
-            rangemin    = boost::lexical_cast<double>(argv[5]);
-        if (argc > 6)
-            rangemax    = boost::lexical_cast<double>(argv[6]);
-        
-        if (argc > 7)
-            variancemin = boost::lexical_cast<double>(argv[7]);
-        if (argc > 8)
-            variancemax = boost::lexical_cast<double>(argv[8]);
-        
-    } catch (...) {
-        throw std::runtime_error("numerical parameter not correct");
-    }    
-
+    std::map<std::string, boost::any> l_args;
+    if (!cliArguments(argc, argv, l_args))
+        return EXIT_FAILURE;
     
     
     // create cloud object and set the data
-    tools::sources::cloud<double> cloud(dim);
-
+    tools::sources::cloud<double> cloud( boost::any_cast<std::size_t>(l_args["dimension"]) );
+    
     // set range of the points
-    cloud.setPoints(pointsmin, pointsmax);
+    cloud.setPoints( boost::any_cast<std::size_t>(l_args["pointsmin"]), boost::any_cast<std::size_t>(l_args["pointsmax"]) );
     
     // set variance, range and samples
-    cloud.setVariance(variancemin, variancemax);
-    for(std::size_t i=0; i < dim; ++i)
-        cloud.setRange(i, rangemin, rangemax, sampling);
+    cloud.setVariance( boost::any_cast<double>(l_args["variancemin"]), boost::any_cast<double>(l_args["variancemax"]) );
+    for(std::size_t i=0; i < boost::any_cast<std::size_t>(l_args["dimension"]); ++i)
+        cloud.setRange(i, boost::any_cast<double>(l_args["rangemin"]), boost::any_cast<double>(l_args["rangemax"]), boost::any_cast<std::size_t>(l_args["sampling"]));
     
-    // create hdf file and generate data
-    tools::files::hdf hdf("cloud.hdf5", true);
-    hdf.write<double>( "/data",  cloud.generate(), H5::PredType::NATIVE_DOUBLE );
-    
-    std::cout << "create HDF file \"cloud.hdf5\" with dataset \"/data\"" << std::endl;
+    // create file and write data to hdf
+    tools::files::hdf target(boost::any_cast<std::string>(l_args["outfile"]), true);
+    target.write<double>( boost::any_cast<std::string>(l_args["outpath"]),  cloud.generate(), H5::PredType::NATIVE_DOUBLE );
+
     return EXIT_SUCCESS;
 }
