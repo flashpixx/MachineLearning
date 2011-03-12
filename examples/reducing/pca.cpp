7d09736b64345694a1d04320c3fbe5f2ee97d84f
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-03-12 18:02:34 +0000

    pca example

--- examples/reducing/pca.cpp
+++ examples/reducing/pca.cpp
@@ -21,7 +21,10 @@
  @endcond
  **/
 
+#include <map>
+
 #include <machinelearning.h>
+#include <boost/any.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 
@@ -30,29 +33,101 @@ namespace dim   = machinelearning::dimensionreduce::nonsupervised;
 namespace tools  = machinelearning::tools;
 
 
-int main(int argc, char* argv[]) {
-    if (argc < 4)
-        throw std::runtime_error("you need at least three parameter as input. first HDF file, second path to dataset, third number of projected dimensions");
+
+bool cliArguments( int argc, char* argv[], std::map<std::string, boost::any>& p_args ) {
     
-    // convert string parameter to numerical data
-    std::size_t targetdim = 0;
-    try {
-        targetdim = boost::lexical_cast<std::size_t>(argv[3]);
-    } catch (...) {
-        throw std::runtime_error("target dimension can not be read");
+    if (argc < 2) {
+        std::cout << "--inputfile" << "\t" << "input HDF5 file" << std::endl;
+        std::cout << "--inputpath" << "\t" << "path to dataset" << std::endl;
+        std::cout << "--outfile" << "\t" << "output HDF5 file" << std::endl;
+        std::cout << "--outpath" << "\t" << "output path within the HDF5 file" << std::endl;
+        std::cout << "--dimension" << "\t" << "number of project dimensions" << std::endl;
+        
+        return false;
     }
     
-    // read source hdf file
-    tools::files::hdf source( argv[1] );
+    
+    // set default arguments
+    std::map<std::string, std::vector<std::string> > l_argmap;
+    l_argmap["inputfile"] = std::vector<std::string>();
+    l_argmap["inputpath"] = std::vector<std::string>();
+    l_argmap["outfile"]   = std::vector<std::string>();
+    l_argmap["outpath"]   = std::vector<std::string>();
+    l_argmap["dimension"] = std::vector<std::string>();
+    
+    
+    // read all arguments
+    std::size_t n=1;
+    for(int i=1; i < argc; i+=n) {
+        n = 1;
+        std::string lc(argv[i]);
+        if (lc.length() < 2)
+            continue;
+        
+        lc = lc.substr(2);
+        boost::to_lower( lc );
+        
+        if (l_argmap.find(lc) != l_argmap.end()) {
+            std::vector<std::string> lv;
+            
+            for(int l=i+1; l < argc; ++l) {
+                std::string lc2(argv[l]);
+                if ( (lc2.length() >= 2) && (lc2.substr(0,2) == "--") )
+                    break;
+                
+                lv.push_back(lc2);
+                n++;
+            }
+            
+            l_argmap[lc] = lv;
+        }
+        
+    }
+    
+    
+    //check map values and convert them
+    if ((l_argmap["dimension"].size() != 1) || 
+        (l_argmap["outfile"].size() != 1) || 
+        (l_argmap["outpath"].size() != 1) || 
+        (l_argmap["inputfile"].size() != 1) || 
+        (l_argmap["inputpath"].size() != 1)
+        )
+        throw std::runtime_error("number of arguments are incorrect");
+    
+    p_args["inputfile"]     = l_argmap["inputfile"][0];
+    p_args["inputpath"]     = l_argmap["inputpath"][0];
+    p_args["outfile"]       = l_argmap["outfile"][0];
+    p_args["outpath"]       = l_argmap["outpath"][0];
+    
+    try {
+        p_args["dimension"]  = boost::lexical_cast<std::size_t>( l_argmap["dimension"][0] );
+    } catch (...) {
+        throw std::runtime_error("numerical data can not extracted");
+    }  
+    
+    return true;
+}
 
+
+
+int main(int argc, char* argv[]) {
+
+    std::map<std::string, boost::any> l_args;
+    if (!cliArguments(argc, argv, l_args))
+        return EXIT_FAILURE;
+    
+    
+    // read source hdf file
+    tools::files::hdf source( boost::any_cast<std::string>(l_args["inputfile"]) );
+    
     // create pca object and map the data
-    dim::pca<double> pca( targetdim );
-    ublas::matrix<double> project = pca.map( source.readMatrix<double>(argv[2], H5::PredType::NATIVE_DOUBLE) );
+    dim::pca<double> pca( boost::any_cast<std::size_t>(l_args["dimension"]) );
+    ublas::matrix<double> project = pca.map( source.readMatrix<double>(boost::any_cast<std::string>(l_args["inputpath"]), H5::PredType::NATIVE_DOUBLE) );
+
     
     // create file and write data to hdf
-    tools::files::hdf target("pca.hdf5", true);
-    target.write<double>( "/data",  project, H5::PredType::NATIVE_DOUBLE );
+    tools::files::hdf target(boost::any_cast<std::string>(l_args["outfile"]), true);
+    target.write<double>( boost::any_cast<std::string>(l_args["outpath"]),  project, H5::PredType::NATIVE_DOUBLE );
     
-    std::cout << "create HDF file \"pca.hdf5\" with dataset \"/data\"" << std::endl;
     return EXIT_SUCCESS;
 }
