a9d7c92febce47ad74e9750ada752ca163118c82
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-04-11 21:45:03 +0000

    doku

--- tools/random.hpp
+++ tools/random.hpp
@@ -65,6 +65,7 @@ namespace machinelearning { namespace tools {
      * system-random-generator can be used. The class holds different distribution that
      * can be used. The system-random-generator must be set with the compileflag 
      * $LastChangedDate$
+     * @todo think about pseudo generator random seed
      **/
     class random {
         
