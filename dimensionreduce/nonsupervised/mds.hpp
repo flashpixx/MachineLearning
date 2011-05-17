8ffcb77a70c91f854e5ca6f8913f3a9b389fe4f4
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-05-17 20:28:37 +0000

    hit mds mpi

--- dimensionreduce/nonsupervised/mds.hpp
+++ dimensionreduce/nonsupervised/mds.hpp
@@ -366,8 +366,7 @@ namespace machinelearning { namespace dimensionreduce { namespace nonsupervised 
         for(std::size_t i=0; i < l_target.size1(); ++i)
             for(std::size_t j=0; j < l_target.size2(); ++j)
                 l_target(i,j) = n++;
-        std::cout << "single\n" << l_target << std::endl;
-        return l_target;
+
         
         // count zero elements
         std::vector< std::pair<std::size_t, std::size_t> > l_zeros;
@@ -540,8 +539,7 @@ namespace machinelearning { namespace dimensionreduce { namespace nonsupervised 
         for(std::size_t i=0; i < l_target.size1(); ++i)
             for(std::size_t j=0; j < l_target.size2(); ++j)
                 l_target(i,j) = l_columnstart*l_dimensionMPI+n++;
-        std::cout << "CPU " << p_mpi.rank() << "\n" << l_target << std::endl;
-        return l_target;
+
         
         // count zero elements
         std::vector< std::pair<std::size_t, std::size_t> > l_zeros;
