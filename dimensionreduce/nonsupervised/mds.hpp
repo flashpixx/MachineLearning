232506ff065f3fa09135606707d5bc3bf4515a89
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-05-19 13:47:03 +0000

    hit mds mpi

--- dimensionreduce/nonsupervised/mds.hpp
+++ dimensionreduce/nonsupervised/mds.hpp
@@ -451,6 +451,9 @@ namespace machinelearning { namespace dimensionreduce { namespace nonsupervised 
                 ublas::matrix<T> l_col = tools::matrix::repeat( static_cast< ublas::vector<T> >(ublas::column(l_target, j)), tools::matrix::column );
                 
                 l_col -= ublas::trans(l_col);
+                std::cout << l_col << std::endl;
+                return l_col;
+                
                 l_col  = ublas::element_prod(l_col, l_strength);
                 
                 ublas::column(l_update, j) = tools::matrix::sum(l_col, tools::matrix::column);
@@ -667,6 +670,9 @@ namespace machinelearning { namespace dimensionreduce { namespace nonsupervised 
                 for(std::size_t n=0; n < l_col.size1(); ++n)
                     ublas::row(l_col, n) -= l_fullrow;
     
+                std::cout << "CPU " << p_mpi.rank() << "\n" << l_col << std::endl;
+                return l_col;
+                
                 l_col  = ublas::element_prod(l_col, l_strength);
                 
                 // we create the column data of the update matrix over all processes
