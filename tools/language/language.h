93de7173a78e69efb43a1a0af210ce5a1abb8086
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-07-22 21:27:51 +0000

    language

--- tools/language/language.h
+++ tools/language/language.h
@@ -24,6 +24,11 @@
 #ifndef MACHINELEARNING_TOOLS_LANGUAGE_H
 #define MACHINELEARNING_TOOLS_LANGUAGE_H
 
+#include <vector>
+#include <boost/algorithm/string.hpp> 
+
+#include "../../exception/exception.h"
+#include "bindings.h"
 
 
 namespace machinelearning { namespace tools { 
@@ -41,12 +46,70 @@ namespace machinelearning { namespace tools {
             #include "iso639.h"
             #undef LANGUAGE_CODE
         };
-    
+        
+        /** enum for creating the code list **/
+        enum listcode {
+            all,
+            iso639_1,
+            iso639_3
+        };
+        
+        
+   
+        /** convert a string into the language code 
+         * @param p_lang string value with language code
+         * @return enum language code
+         **/
+        inline code fromString( const std::string& p_lang ) {
+            if (p_lang.empty() || (p_lang.size() < 2) || (p_lang.size() > 3))
+                throw exception::runtime(_("parameter is not a language code"));
+            
+            std::string lc = p_lang;
+            boost::to_lower(lc);
+            
+            #define LANGUAGE_CODE( iso6391, iso6393, description )      if (lc == #iso6391) return iso6391; if (lc == #iso6393) return iso6393;
+            #include "iso639.h"
+            #undef LANGUAGE_CODE
+            
+            throw exception::runtime(_("language is not known"));
+        }
+        
+        
+        /** converts a enum type to the string representation
+         * @param language code
+         * @param enum option for returning iso639-1 or iso639-3 representation
+         * @return string
+         **/
+        inline std::string toString( const code& p_lang, const listcode& p_return = iso639_1 ) {
+            
+            switch (p_lang) {
+                    
+                #define LANGUAGE_CODE( iso6391, iso6393, description )      case iso6391 : return (p_return == iso639_3) ? #iso6393 : #iso6391;
+                #include "iso639.h"
+                #undef LANGUAGE_CODE
+                    
+                default :
+                    throw exception::runtime(_("language is not known"));
+            }
+        }
+        
+        
+        /** return a list with all language codes
+         * @param p_list option for creating code list
+         * @return vector with language codes
+         **/
+        inline std::vector<std::string> getCodeList( const listcode& p_list = all ) {
+            std::vector<std::string> l_list;
+            
+            #define LANGUAGE_CODE( iso6391, iso6393, description )      if ((p_list == all) || (p_list == iso639_1)) l_list.push_back(#iso6391); if ((p_list == all) || (p_list == iso639_3)) l_list.push_back(#iso6393);
+            #include "iso639.h"
+            #undef LANGUAGE_CODE
+            
+            return l_list;
+        }
+        
     };
 
 };};
-
-
-#include "bindings.h"
 
 #endif
