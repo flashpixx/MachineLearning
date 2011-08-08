25f026ec2a1aa8d7266187903488fb4d570ce5e4
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-08-08 16:25:53 +0000

    example newsgroup via boost program oboption

--- examples/sources/newsgroup.cpp
+++ examples/sources/newsgroup.cpp
@@ -27,91 +27,12 @@
 #include <machinelearning.h>
 #include <boost/any.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/options_description.hpp>
 
 using namespace machinelearning;
-
-
-/** read all input arguments 
- * @param argc number of arguments of "main"
- * @param argv arguments of "main"
- * @param p_args map with argument values (default values)
- * @return bool if all is correct
- **/
-bool cliArguments( int argc, char* argv[], std::map<std::string, boost::any>& p_args ) {
-    
-    if (argc < 2) {
-        std::cout << "--server \t\t IP / address of the NNTP server" << std::endl;
-        std::cout << "--groups \t\t list of groups or empty for getting group list" << std::endl;
-        std::cout << "--content \t\t content of articles (values: full, body [default], header)" << std::endl;
-        std::cout << "--canceled \t\t show canceled articles (vales: off [default], on)" << std::endl;
-        return false;
-    }
-    
-    // set default arguments
-    std::map<std::string, std::vector<std::string> > l_argmap;
-    l_argmap["server"]     = std::vector<std::string>();
-    l_argmap["groups"]     = std::vector<std::string>();
-    l_argmap["content"]    = std::vector<std::string>();
-    l_argmap["canceled"]   = std::vector<std::string>();
-    
-    
-    // read all arguments
-    std::size_t n=1;
-    for(int i=1; i < argc; i+=n) {
-        n = 1;
-        std::string lc(argv[i]);
-        if (lc.length() < 2)
-            continue;
-        
-        lc = lc.substr(2);
-        boost::to_lower( lc );
-        
-        if (l_argmap.find(lc) != l_argmap.end()) {
-            std::vector<std::string> lv;
-            
-            for(int l=i+1; l < argc; ++l) {
-                std::string lc2(argv[l]);
-                if ( (lc2.length() >= 2) && (lc2.substr(0,2) == "--") )
-                    break;
-                
-                lv.push_back(lc2);
-                n++;
-            }
-            
-            l_argmap[lc] = lv;
-        }
-        
-    }
-    
-    
-    //check map values and convert them
-    if ( (l_argmap["server"].size() != 1) )
-        throw std::runtime_error("number of arguments are incorrect");
-    
-    p_args["server"]      = l_argmap["server"][0];
-    p_args["groups"]      = l_argmap["groups"];
-    p_args["canceled"]    = false;
-    
-    p_args["content"]     = tools::sources::nntp::body;
-    if (l_argmap["content"].size() > 0) {
-        std::string lc = l_argmap["content"][0];
-        boost::to_lower(lc);
-    
-        if (lc == "full")
-            p_args["content"]     = tools::sources::nntp::full;
-        if (lc == "header")
-            p_args["content"]     = tools::sources::nntp::header;
-    }
-    
-    if (l_argmap["canceled"].size() > 0) {
-        std::string lc = l_argmap["canceled"][0];
-        boost::to_lower(lc);
-        p_args["canceled"] = lc == "on";
-    }
-    
-    
-    return true;
-}
+namespace po = boost::program_options;
 
 
 /** main program
@@ -119,17 +40,42 @@ bool cliArguments( int argc, char* argv[], std::map<std::string, boost::any>& p_
  * @param argv arguments
  **/
 int main(int argc, char* argv[]) {
+  
+    std::string l_content;
+    bool l_cancel;
     
-    std::map<std::string, boost::any> l_args;
-    if (!cliArguments(argc, argv, l_args))
+    // create CML options with description
+    po::options_description l_description("allowed options");
+    l_description.add_options()
+        ("help", "produce help message")
+        ("server", po::value<std::string>(), "IP / address of the NNTP server")
+        ("groups", po::value<std::string>(), "list of groups / not set = show group list")
+        ("content", po::value<std::string>(&l_content)->default_value("body"), "content of articles (values: full, body [default], header)")
+        ("canceled", po::value<bool>(&l_cancel)->default_value(false), "show canceled articles (values: false / 0 [default], true)")
+    ;
+    
+    po::variables_map l_map;
+    po::store(po::parse_command_line(argc, argv, l_description), l_map);
+    po::notify(l_map);
+    
+    if (l_map.count("help")) {
+        std::cout << l_description << std::endl;
+        return EXIT_SUCCESS;
+    }
+    
+    if (!l_map.count("server")) {
+        std::cout << "[--server] option must be set" << std::endl;
         return EXIT_FAILURE;
-            
+    }
+    
+    
+    
             
     // connect to server
-    tools::sources::nntp news( boost::any_cast<std::string>(l_args["server"]) );
+    tools::sources::nntp news( l_map["server"].as<std::string>() );
 
-    // on one parameter we return the group list
-            if ( (boost::any_cast< std::vector<std::string> >(l_args["groups"])).size() == 0) {
+    if (!l_map.count("groups")) {
+
         // read group list
         std::map<std::string, std::size_t> groups = news.getGroupList();
         for (std::map<std::string, std::size_t>::iterator it = groups.begin(); it != groups.end(); ++it)
@@ -137,17 +83,23 @@ int main(int argc, char* argv[]) {
     
     } else {
     
-        // sets which part of a messages is read
-        news.setContent( boost::any_cast<tools::sources::nntp::content>(l_args["content"]) );
+        news.setContent( tools::sources::nntp::body );
+        if (l_content == "full")
+            news.setContent( tools::sources::nntp::full );
+        if (l_content == "header")
+            news.setContent( tools::sources::nntp::header );
+        
+
+        std::vector<std::string> l_groups;
+        boost::split( l_groups, l_map["groups"].as<std::string>(), boost::is_any_of(",") );
     
-        std::vector<std::string> lgroups = boost::any_cast< std::vector<std::string> >(l_args["groups"]);
-        for(std::size_t i=0; i < lgroups.size(); ++i) {
+        for(std::size_t i=0; i < l_groups.size(); ++i) {
             // sets the newsgroup for browsing 
-            news.setGroup( lgroups[i] );
+            news.setGroup( l_groups[i] );
     
             // browse each article in the group
             for(tools::sources::nntp::iterator it=news.begin(); it != news.end(); ++it) {
-                if ( (it->isArticleCanceled()) && (!boost::any_cast<bool>(l_args["canceled"])) )
+                if ( (it->isArticleCanceled()) && l_cancel )
                     continue;
                 
                 std::cout << it->getArticle()  << "\n===================================================================================" << std::endl;
