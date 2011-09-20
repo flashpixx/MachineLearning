db711ef1ca7a3e4b6787f250703c4f47df6249fc
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-09-20 11:28:30 +0000

    ga crossover

---
+++ geneticalgorithm/crossover/crossover.h
@@ -0,0 +1,41 @@
+/** 
+ @cond
+ ############################################################################
+ # LGPL License                                                             #
+ #                                                                          #
+ # This file is part of the Machine Learning Framework.                     #
+ # Copyright (c) 2010, Philipp Kraus, <philipp.kraus@flashpixx.de>          #
+ # This program is free software: you can redistribute it and/or modify     #
+ # it under the terms of the GNU Lesser General Public License as           #
+ # published by the Free Software Foundation, either version 3 of the       #
+ # License, or (at your option) any later version.                          #
+ #                                                                          #
+ # This program is distributed in the hope that it will be useful,          #
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+ # GNU Lesser General Public License for more details.                      #
+ #                                                                          #
+ # You should have received a copy of the GNU Lesser General Public License #
+ # along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+ ############################################################################
+ @endcond
+ **/
+
+
+#ifndef __MACHINELEARNING_GENETICALGORITHM_CROSSOVER_CROSSOVER_H
+#define __MACHINELEARNING_GENETICALGORITHM_CROSSOVER_CROSSOVER_H
+
+namespace machinelearning { namespace geneticalgorithm { 
+    
+    /** namespace of the genetic algorithms for crossover structures
+     * $LastChangedDate: 2011-09-20 11:32:42 +0200 (Di, 20 Sep 2011) $
+     **/
+    namespace crossover {};
+    
+};};
+
+#include "crossover.hpp"
+#include "kcrossover.hpp"
+
+#endif
+
