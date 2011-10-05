b791b44271af66718d8303375b0d03d3ca3cbe6c
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-10-05 10:12:07 +0000

    java exception

---
+++ java/machinelearning/exception/unknowntype.java
@@ -0,0 +1,43 @@
+/** 
+ @cond
+ ############################################################################
+ # LGPL License                                                             #
+ #                                                                          #
+ # This file is part of the Machine Learning Framework.                     #
+ # Copyright (c) 2010, Philipp Kraus, <philipp.kraus@flashpixx.de>          #
+ # This program is free software: you can redistribute it and/or modify     #
+ # it under the terms of the GNU Lesser General Public License as           #
+ # published by the Free Software Foundation, either version 3 of the       #
+ # License, or (at your option) any later version.                          #
+ #                                                                          #
+ # This program is distributed in the hope that it will be useful,          #
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+ # GNU Lesser General Public License for more details.                      #
+ #                                                                          #
+ # You should have received a copy of the GNU Lesser General Public License #
+ # along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+ ############################################################################
+ @endcond
+ **/
+
+package machinelearning.exception;
+
+
+
+/** classmethod exception class equal to the C++ exception
+ * $LastChangedDate$
+ **/
+public class unknowntype extends IllegalArgumentException {
+    
+    
+    /** constructor **/
+    public unknowntype() { super(); }
+    
+    /** constructor
+     * @apram p_msg message
+     **/
+    public unknowntype(String p_msg) { super(p_msg); }
+    
+    
+}
