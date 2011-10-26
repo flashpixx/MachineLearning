a4275600e53b47d441fbb7c3dd3ed347934530cc
Philipp Kraus <philipp.kraus@flashpixx.de>
2011-10-26 15:32:35 +0000

    docu

--- machinelearning.h
+++ machinelearning.h
@@ -48,7 +48,7 @@
  *         <li><i>optional MPI support</i></li>
  *     </ul>
  * </li>
- * <li>Boost Bindings (SVN http://svn.boost.org/svn/boost/sandbox/numeric_bindings/ )</li>
+ * <li>Boost Bindings ( SVN http://svn.boost.org/svn/boost/sandbox/numeric_bindings/ )</li>
  * <li>Linear Algebra PACKage ( http://www.netlib.org/lapack/ ) [Version newer 3.3]</li>
  * <li><i>optional Hierarchical Data Format (HDF)</i> ( http://www.hdfgroup.org/ ) [version newer or equal 5.1.8.4]</li>
  * <li><i>optional Message-Passing-Interface-Support</i>
