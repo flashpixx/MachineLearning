d58f380e4e5bddcd99ebe42a2b6dd3713d73fdf8
Philipp Kraus <philipp.kraus@flashpixx.de>
2012-08-02 19:34:56 +0000

    docu

--- machinelearning.h
+++ machinelearning.h
@@ -237,7 +237,7 @@
  * must be run. After that Boost can be build with (The MPI support can be enabled with <dfn>withmpi</dfn>, but it requires MPI sources and libraries. The configuration for MPI use can be found on
  * <dfn>www.boost.org/doc/libs/-release number-/doc/html/mpi.html</dfn>)
  * @code bjam --with-exception --with-filesystem --with-math --with-random --with-regex --with-date_time --with-thread --with-system --with-program_options --with-serialization --with-iostreams --disable-filesystem2 threading=multi runtime-link=shared variant=release toolset=gcc|darwin install @endcode
- * In newer Boost version (since 1.47.0) the <dfn>bjam</dfn> command is replaced with <dfn>b2</dfn>. The <dfn>toolset</dfn> option must be:
+ * In newer Boost version (since 1.50) the <dfn>bjam</dfn> command is replaced with <dfn>b2</dfn>. The <dfn>toolset</dfn> option must be:
  * <ul>
  * <li><dfn>gcc</dfn> for unix systems (Cygwin too)</li>
  * <li><dfn>darwin</dfn> for Mac OS X</li>
