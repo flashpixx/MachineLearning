e5bf096d980a2a37c59c8970bae292ccf1813073
Philipp Kraus <philipp.kraus@flashpixx.de>
2012-12-02 23:04:00 +0100

    ifdef error fix

--- tools/language/language.h
+++ tools/language/language.h
@@ -21,8 +21,8 @@
  @endcond
  **/
 
-#ifndef __MACHINELEARNING_TOOLS_LANGUAGE_H
-#define __MACHINELEARNING_TOOLS_LANGUAGE_H
+#ifndef __MACHINELEARNING_TOOLS_LANGUAGE_LANGUAGE_H
+#define __MACHINELEARNING_TOOLS_LANGUAGE_LANGUAGE_H
 
 #include <vector>
 #include <boost/algorithm/string.hpp> 
